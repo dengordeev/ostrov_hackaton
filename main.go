68f288d69e7c8bf11f62b6ea261af1c20504ba89
Den Gordeev <gordeevden15@gmail.com>
2018-07-17 14:57:24 +0300

    test

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 //User - структура, содержащая данные о пользователе
 type User struct {
@@ -15,20 +20,28 @@ type User struct {
 
 //Project - структура, содержащая данные о конкретном проекте
 type Project struct {
-	ID     int      `json:"id"`
-	IsOpen bool     `json:"isopen"`
-	Name   string   `json:"name"`
-	About  string   `json:"about"`
-	Part   []*Users `json:"party"`
+	ID     int     `json:"id"`
+	IsOpen bool    `json:"isopen"`
+	Name   string  `json:"name"`
+	About  string  `json:"about"`
+	Part   []*User `json:"party"`
 }
 
-type Users []*User
-type Projects []*Project
-
 var (
-	users    = new(Users)
-	projects = new(Projects)
+	users    = []*User{}
+	projects = []*Project{}
 )
+
+func findTeamLeaders(users []*User) []*User {
+	result := []*User{}
+	for _, u := range users {
+		if len(u.MyProjects) != 0 {
+			result = append(result, u)
+		}
+	}
+
+	return result
+}
 
 func main() {
 	r := gin.Default()
@@ -41,9 +54,70 @@ func main() {
 
 	})
 	*/
-	r.Any("/projects", func(c *gin.Context) {
+	u1 := &User{
+		1,
+		"dengordeev",
+		"Гордеев",
+		"Денис",
+		"",
+		[]*Project{},
+		[]*Project{},
+	}
+	u2 := &User{
+		1,
+		"user",
+		"User",
+		"User",
+		"I'm user",
+		[]*Project{},
+		[]*Project{},
+	}
+	p1 := &Project{
+		1,
+		true,
+		"Первый проект",
+		"Он лучший",
+		[]*User{},
+	}
+	projects = append(projects, p1)
+	users = append(users, u1, u2)
+	r.GET("/projects", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"projects": projects,
+		})
+	})
+	r.PUT("/projects", func(c *gin.Context) {
+		project := &Project{}
+		err := c.BindJSON(&project)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, nil)
+		}
+		project.ID = len(projects) + 1
+		projects = append(projects, project)
+
+		c.JSON(http.StatusOK, nil)
+	})
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"users": users,
+		})
+	})
+	r.POST("/users/add", func(c *gin.Context) {
+		user := &User{}
+		err := c.BindJSON(&user)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, nil)
+		}
+		user.ID = len(users) + 1
+		users = append(users, user)
+
+		c.JSON(http.StatusOK, nil)
+	})
+	r.GET("/teamleaders", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"teamleaders": findTeamLeaders(users),
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
