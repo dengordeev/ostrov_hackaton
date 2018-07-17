5a9416ef781de8e6a13514c006bd25005ef89c7b
Den Gordeev <gordeevden15@gmail.com>
2018-07-17 13:43:50 +0300

    add static

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+
+	r.Static("/app", "./static")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
