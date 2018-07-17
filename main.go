6acdc39962750b14307552c241c9f1a1e0b65a06
Den Gordeev <gordeevden15@gmail.com>
2018-07-17 14:08:33 +0300

    test

--- main.go
+++ main.go
@@ -2,13 +2,48 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+//User - структура, содержащая данные о пользователе
+type User struct {
+	ID         int        `json:"id"`
+	Username   string     `json:"username"`
+	Lastname   string     `json:"lastname"`
+	Firstname  string     `json:"firsname"`
+	About      string     `json:"about"`
+	MyProjects []*Project `json:"myprojects"`
+	Projects   []*Project `json:"projects"`
+}
+
+//Project - структура, содержащая данные о конкретном проекте
+type Project struct {
+	ID     int      `json:"id"`
+	IsOpen bool     `json:"isopen"`
+	Name   string   `json:"name"`
+	About  string   `json:"about"`
+	Part   []*Users `json:"party"`
+}
+
+type Users []*User
+type Projects []*Project
+
+var (
+	users    = new(Users)
+	projects = new(Projects)
+)
+
 func main() {
 	r := gin.Default()
 
 	r.Static("/app", "./static")
-	r.GET("/ping", func(c *gin.Context) {
+	/*r.POST("/teamleaders", func(c *gin.Context) {
+
+	})
+	r.POST("/users", func(c *gin.Context) {
+
+	})
+	*/
+	r.Any("/projects", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"projects": projects,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
