c92eed0edb4a491476fdf8f82462bc21f12de15d
Tinylionet <ucckrox@gmail.com>
2018-07-18 23:39:36 +1000

    pro

--- main.go
+++ main.go
@@ -168,7 +168,7 @@ func main() {
 			"users": users,
 		})
 	})
-	r.PUT("/users", func(c *gin.Context) {
+	r.POST("/users", func(c *gin.Context) {
 		user := &User{}
 		err := c.BindJSON(&user)
 		log.Println(user)
@@ -182,6 +182,25 @@ func main() {
 
 		c.JSON(http.StatusOK, nil)
 	})
+
+	r.POST("/project/:id", func(c *gin.Context) {
+		tid, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, nil)
+			return
+		}
+		for _, t := range projects {
+			if t.ID == tid {
+				c.JSON(http.StatusOK, gin.H{
+					"project": t,
+				})
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, nil)
+	})
+
 	r.GET("/teamleaders", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"teamleaders": findTeamLeaders(users),
